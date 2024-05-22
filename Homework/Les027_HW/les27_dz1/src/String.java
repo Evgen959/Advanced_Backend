009f7b5563ba0397c4bebb0354f692351afc001f
Evgen959 <evgen.tm959@gmail.com>
2024-05-22 09:38:23 +0200

    newLes28

--- Homework/Les027_HW/les27_dz1/src/String.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class String implements Comparator<java.lang.String> {
-    @Override
-    public int compare(java.lang.String o1, java.lang.String o2) {
-        return 0;
-    }
-}
