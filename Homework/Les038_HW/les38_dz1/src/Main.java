2cd0af9e3b43dd31ce99afd3009cfb7da5c108cd
Evgen959 <evgen.tm959@gmail.com>
2024-06-05 15:58:52 +0200

    newNW1Les38

---
+++ Homework/Les038_HW/les38_dz1/src/Main.java
@@ -0,0 +1,43 @@
+/* 1
+Предположим, вы пишете программу учета студентов компьютерных курсов.
+У вас есть списки студентов нескольких групп (т.е. несколько List<Student>).
+Некоторые студенты посещают занятия в нескольких группах.
+Ваша задача получить список (List) всех студентов школы.*/
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> group01 = new ArrayList<>();
+        group01.add(new Student("Jack", "Vorobey"));
+        group01.add(new Student("Ann", "Hatuay"));
+        group01.add(new Student("Ivan", "Ivanov"));
+        group01.add(new Student("Josher", "Gimps"));
+        group01.add(new Student("Nick", "Nikolson"));
+        List<Student> group02 = new ArrayList<>();
+        group02.add(new Student("Jan", "Reno"));
+        group02.add(new Student("Ann", "Hatuay"));
+        group02.add(new Student("Lena", "Kushnarova"));
+        group02.add(new Student("Josher", "Gimps"));
+        List<Student> group03 = new ArrayList<>();
+        group03.add(new Student("July", "Roberc"));
+        group03.add(new Student("Vasiliy", "Vishnevskiy"));
+        group03.add(new Student("Nikl", "Nikolson"));
+        group03.add(new Student("Josher", "Gimps"));
+
+        System.out.println("--------studentSet----------");
+        Set<Student> studentSet = new HashSet<>();
+        studentSet.addAll(group01);
+        studentSet.addAll(group02);
+        studentSet.addAll(group03);
+        System.out.println(studentSet);
+
+        System.out.println("---------studentList---------");
+        List<Student> studentList = new ArrayList<>(studentSet);
+        System.out.println(studentList);
+    }
+}
---
+++ Homework/Les038_HW/les38_dz3/src/Main.java
@@ -0,0 +1,15 @@
+/*3
+Предположим у вас есть List пользователей вашего сайта User(String nickname, String email)
+Ваша задача написать метод, который проверит,
+может ли быть зарегистрирован User с заданным email или с заданным nickname.
+Естественно, и email и nickname должны быть уникальны.*/
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
