65e4e1e9b7c830a94d21dcf5b79685e7f22c483c
Evgen959 <evgen.tm959@gmail.com>
2024-06-14 12:46:19 +0200

    newKonsulLes45

--- konsul/les045/src/Person.java
+++ konsul/les045/src/Person.java
@@ -1,47 +1,27 @@
 import java.util.Objects;
 
 public class Person {
-    private String firstName;
-    private String lastName;
+    private String name;
     private int age;
 
-    public Person(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Person(String name, int age) {
+        this.name = name;
         this.age = age;
     }
 
     @Override
     public String toString() {
         return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
+    public String getName() {
+        return name;
     }
 
     public int getAge() {
         return age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName, lastName, age);
     }
 }
